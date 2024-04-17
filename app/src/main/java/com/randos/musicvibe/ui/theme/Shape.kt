ef2f7dc6fceb05a75c731e8264ed38db480f43e4
Vishal Kumar <vsnappy1@gmail.com>
2024-04-16 21:16:56 -0700

    This is where it all begins...

---
+++ app/src/main/java/com/randos/musicvibe/ui/theme/Shape.kt
@@ -0,0 +1,13 @@
+package com.randos.musicvibe.ui.theme
+
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Shapes
+import androidx.compose.ui.unit.dp
+
+val Shapes = Shapes(
+    extraSmall = RoundedCornerShape(2.0.dp),
+    small = RoundedCornerShape(4.0.dp),
+    medium = RoundedCornerShape(8.0.dp),
+    large = RoundedCornerShape(12.0.dp),
+    extraLarge = RoundedCornerShape(16.0.dp)
+)
